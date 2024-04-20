cc1c6459f03c53baa0b85df4d51e91a17d5cbbbc
Dimas Novandra Utomo <dimasutomo@dimasutomo.com>
2024-04-21 00:31:10 +0700

    set deploy to the ap-southeast-1 region

--- __main__.py
+++ __main__.py
@@ -3,6 +3,9 @@ from vpc import vpc, subnet, igw, route_table, route_table_assoc
 from bastion_host import bastion, bastion_sg
 from eks import cluster, node_group
 
+# Create an AWS provider instance with the specified region
+aws_provider = aws.Provider("aws", region="ap-southeast-1")
+
 pulumi.export("cluster_name", cluster.name)
 pulumi.export("node_group_name", node_group.node_group_name)
 pulumi.export("bastion_ip", bastion.public_ip)
