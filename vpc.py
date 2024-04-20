ffc627361e6f9a57b238361468501cc9444032a3
Dimas Novandra Utomo <dimasutomo@dimasutomo.com>
2024-04-21 01:20:02 +0700

    fix cidr

--- vpc.py
+++ vpc.py
@@ -20,7 +20,7 @@ igw = aws.ec2.InternetGateway("my-igw",
 # Create Subnets in different Availability Zones
 subnet1 = aws.ec2.Subnet("my-subnet-1",
     vpc_id=vpc.id,
-    cidr_block="10.0.1.0/24",
+    cidr_block="10.0.10.0/24",  # Updated non-conflicting CIDR block
     availability_zone="ap-southeast-1a",
     map_public_ip_on_launch=True,
     tags={
@@ -29,7 +29,7 @@ subnet1 = aws.ec2.Subnet("my-subnet-1",
 
 subnet2 = aws.ec2.Subnet("my-subnet-2",
     vpc_id=vpc.id,
-    cidr_block="10.0.2.0/24",
+    cidr_block="10.0.20.0/24",  # Updated non-conflicting CIDR block
     availability_zone="ap-southeast-1b",
     map_public_ip_on_launch=True,
     tags={
