d9df7f81427889e6a40f2589ff8ced121782e2c9
Dimas Novandra Utomo <dimasutomo@dimasutomo.com>
2024-04-21 00:54:22 +0700

    change ami id and create another one az for eks

--- eks.py
+++ eks.py
@@ -1,19 +1,20 @@
 import pulumi
 import pulumi_aws as aws
-from vpc import subnet, vpc
+from vpc import vpc, subnet1, subnet2
 
 # IAM Role for EKS
 eks_role = aws.iam.Role("eks-role",
-    assume_role_policy="""{
+    assume_role_policy=json.dumps({
         "Version": "2012-10-17",
         "Statement": [{
             "Action": "sts:AssumeRole",
-            "Effect": "Allow",
             "Principal": {
                 "Service": "eks.amazonaws.com"
-            }
+            },
+            "Effect": "Allow",
+            "Sid": ""
         }]
-    }""")
+    }))
 
 # Attach IAM Policies to the Role
 aws.iam.RolePolicyAttachment("eks-cluster-policy",
@@ -28,14 +29,14 @@ aws.iam.RolePolicyAttachment("eks-service-policy",
 cluster = aws.eks.Cluster("my-cluster",
     role_arn=eks_role.arn,
     vpc_config=aws.eks.ClusterVpcConfigArgs(
-        subnet_ids=[subnet.id]
+        subnet_ids=[subnet1.id, subnet2.id]  # Use subnets from different AZs
     ))
 
 # Create a Node Group
 node_group = aws.eks.NodeGroup("my-node-group",
     cluster_name=cluster.name,
     node_role_arn=eks_role.arn,
-    subnet_ids=[subnet.id],
+    subnet_ids=[subnet1.id, subnet2.id],
     scaling_config=aws.eks.NodeGroupScalingConfigArgs(
         desired_size=2,
         max_size=3,
--- vpc.py
+++ vpc.py
@@ -6,14 +6,37 @@ vpc = aws.ec2.Vpc("my-vpc",
     cidr_block="10.0.0.0/16",
     enable_dns_support=True,
     enable_dns_hostnames=True,
-    tags={"Name": "my-vpc"})
+    tags={
+        "Name": "my-vpc",
+    })
 
-# Create an Internet Gateway
+# Create Internet Gateway
 igw = aws.ec2.InternetGateway("my-igw",
     vpc_id=vpc.id,
-    tags={"Name": "my-internet-gateway"})
+    tags={
+        "Name": "my-igw",
+    })
 
-# Create a Route Table
+# Create Subnets in different Availability Zones
+subnet1 = aws.ec2.Subnet("my-subnet-1",
+    vpc_id=vpc.id,
+    cidr_block="10.0.1.0/24",
+    availability_zone="ap-southeast-1a",
+    map_public_ip_on_launch=True,
+    tags={
+        "Name": "my-subnet-1",
+    })
+
+subnet2 = aws.ec2.Subnet("my-subnet-2",
+    vpc_id=vpc.id,
+    cidr_block="10.0.2.0/24",
+    availability_zone="ap-southeast-1b",
+    map_public_ip_on_launch=True,
+    tags={
+        "Name": "my-subnet-2",
+    })
+
+# Create a Route Table and Associate it with Subnets
 route_table = aws.ec2.RouteTable("my-route-table",
     vpc_id=vpc.id,
     routes=[
@@ -22,16 +45,16 @@ route_table = aws.ec2.RouteTable("my-route-table",
             gateway_id=igw.id,
         )
     ],
-    tags={"Name": "my-route-table"})
+    tags={
+        "Name": "my-route-table",
+    })
 
-# Create a Subnet
-subnet = aws.ec2.Subnet("my-subnet",
-    vpc_id=vpc.id,
-    cidr_block="10.0.1.0/24",
-    map_public_ip_on_launch=True,
-    tags={"Name": "my-subnet"})
-
-# Associate the Subnet with the Route Table
-route_table_assoc = aws.ec2.RouteTableAssociation("my-route-table-assoc",
+# Association for the first subnet
+route_table_assoc1 = aws.ec2.RouteTableAssociation("my-route-table-assoc1",
     route_table_id=route_table.id,
-    subnet_id=subnet.id)
+    subnet_id=subnet1.id)
+
+# Association for the second subnet
+route_table_assoc2 = aws.ec2.RouteTableAssociation("my-route-table-assoc2",
+    route_table_id=route_table.id,
+    subnet_id=subnet2.id)
