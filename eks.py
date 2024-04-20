7c1cf8d6955ffdd1d67d159a1263f3766b6fda6a
Dimas Novandra Utomo <dimasutomo@dimasutomo.com>
2024-04-21 01:51:44 +0700

    fix iam role

--- eks.py
+++ eks.py
@@ -1,58 +1,53 @@
 import pulumi
 import pulumi_aws as aws
-from vpc import vpc, subnet1, subnet2
-import json  # Import the json module
+from vpc import vpc, subnet
 
-# IAM Role for EKS
 eks_role = aws.iam.Role("eks-role",
-    assume_role_policy=json.dumps({  # json.dumps is used to convert Python dictionary to JSON string
+    assume_role_policy=json.dumps({
         "Version": "2012-10-17",
         "Statement": [{
-            "Action": "sts:AssumeRole",
+            "Effect": "Allow",
             "Principal": {
                 "Service": "eks.amazonaws.com"
             },
+            "Action": "sts:AssumeRole"
+        }, {
             "Effect": "Allow",
-            "Sid": ""
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
         }]
     }),
-    tags={
-        "Name": "eks-role"
-    }
-)
+    tags={"Name": "eks-role"})
 
-# Attach IAM Policies to the Role
+# Attach IAM policies required for EKS
 aws.iam.RolePolicyAttachment("eks-cluster-policy",
     role=eks_role.id,
-    policy_arn="arn:aws:iam::aws:policy/AmazonEKSClusterPolicy")
+    policy_arn=aws.iam.get_policy(arn="arn:aws:iam::aws:policy/AmazonEKSClusterPolicy").arn)
 
-aws.iam.RolePolicyAttachment("eks-service-policy",
+aws.iam.RolePolicyAttachment("eks-vpc-resource-controller",
     role=eks_role.id,
-    policy_arn="arn:aws:iam::aws:policy/AmazonEKSServicePolicy")
+    policy_arn=aws.iam.get_policy(arn="arn:aws:iam::aws:policy/AmazonEKSVPCResourceController").arn)
 
-# EKS Cluster
+# Create an EKS Cluster
 cluster = aws.eks.Cluster("my-cluster",
     role_arn=eks_role.arn,
     vpc_config=aws.eks.ClusterVpcConfigArgs(
-        subnet_ids=[subnet1.id, subnet2.id] 
+        subnet_ids=[subnet1.id, subnet2.id]
     ),
-    tags={
-        "Name": "my-eks-cluster"
-    }
-)
+    tags={"Name": "my-eks-cluster"})
 
-# EKS Node Group
+# Create an EKS Node Group
 node_group = aws.eks.NodeGroup("my-node-group",
     cluster_name=cluster.name,
     node_role_arn=eks_role.arn,
-    subnet_ids=[subnet1.id, subnet2.id],  
+    subnet_ids=[subnet1.id, subnet2.id],
     scaling_config=aws.eks.NodeGroupScalingConfigArgs(
         desired_size=2,
         max_size=3,
         min_size=1
     ),
     disk_size=20,
-    tags={
-        "Name": "my-node-group"
-    }
-)
+    instance_types=["t3.small"],
+    tags={"Name": "my-eks-node-group"})
