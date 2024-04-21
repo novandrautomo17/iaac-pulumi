b908757538d4d73fff15b935f2228e8075e161a3
Dimas Novandra Utomo <dimasutomo@dimasutomo.com>
2024-04-21 11:40:29 +0700

    fix sg for eks to prevent Instances failed to join the kubernetes cluster issue

--- vpc.py
+++ vpc.py
@@ -24,7 +24,8 @@ subnet_public = aws.ec2.Subnet("my-subnet-public",
     availability_zone="ap-southeast-1a",
     map_public_ip_on_launch=True,  # Enables public IP assignment
     tags={
-        "Name": "my-public-subnet",
+    "Name": "my-public-subnet",
+    "kubernetes.io/cluster/my-cluster": "shared"
     })
 
 # Private Subnet (for EKS Node Group)
@@ -32,15 +33,16 @@ subnet_private_1 = aws.ec2.Subnet("subnet-private-1",
     vpc_id=vpc.id,
     cidr_block="10.0.31.0/24",
     availability_zone="ap-southeast-1a",  # First AZ
-    tags={"Name": "my-private-subnet-1"})
+    tags={
+    "Name": "my-private-subnet-1",
+    "kubernetes.io/cluster/my-cluster": "shared"  # Replace 'my-cluster' with the actual cluster name
+    })
 
 subnet_private_2 = aws.ec2.Subnet("subnet-private-2",
     vpc_id=vpc.id,
     cidr_block="10.0.41.0/24",
     availability_zone="ap-southeast-1b",  # Second AZ
     tags={"Name": "my-private-subnet-2"})
-
-
 
 # Create a Route Table for Public Subnet
 route_table_public = aws.ec2.RouteTable("my-route-table-public",
@@ -81,7 +83,27 @@ route_table_private = aws.ec2.RouteTable("my-route-table-private",
         "Name": "my-route-table-private",
     })
 
-# Associate Private Subnet with Route Table
-route_table_assoc_private = aws.ec2.RouteTableAssociation("my-route-table-assoc-private",
+# Associate the first private subnet
+aws.ec2.RouteTableAssociation("my-route-table-assoc-private-1",
     route_table_id=route_table_private.id,
     subnet_id=subnet_private_1.id)
+
+# Associate the second private subnet
+aws.ec2.RouteTableAssociation("my-route-table-assoc-private-2",
+    route_table_id=route_table_private.id,
+    subnet_id=subnet_private_2.id)
+
+# Create a security group for the EKS nodes
+eks_node_sg = aws.ec2.SecurityGroup('eks-node-sg',
+    vpc_id=vpc.id,
+    description='Security group for EKS nodes',
+    ingress=[
+        {'protocol': '-1', 'from_port': 0, 'to_port': 0, 'cidr_blocks': ['0.0.0.0/0']},
+    ],
+    egress=[
+        {'protocol': '-1', 'from_port': 0, 'to_port': 0, 'cidr_blocks': ['0.0.0.0/0']},
+    ],
+    tags={
+        "Name": "eks-node-sg"
+    }
+)
