1a1ebb48851eb919d1648df1a0c77b744bc0a528
Dimas Novandra Utomo <dimasutomo@dimasutomo.com>
2024-05-09 21:37:39 +0700

    fix ami id

--- bastion_host.py
+++ bastion_host.py
@@ -2,6 +2,11 @@ import pulumi
 import pulumi_aws as aws
 from vpc import subnet_public, subnet_private_1, subnet_private_2, vpc
 
+# Fetch the latest Amazon Linux 2 AMI
+ami = aws.ec2.get_ami(most_recent=True,
+                      owners=["137112412989"],  # Amazon's owner ID for Amazon Linux 2
+                      filters=[{"name":"name","values":["amzn2-ami-hvm-*-x86_64-gp2"]}])
+
 # Security Group for the Bastion Host
 bastion_sg = aws.ec2.SecurityGroup("bastion-sg",
     vpc_id=vpc.id,
@@ -27,7 +32,7 @@ bastion_sg = aws.ec2.SecurityGroup("bastion-sg",
 bastion = aws.ec2.Instance("my-bastion",
     instance_type="t2.micro",
     vpc_security_group_ids=[bastion_sg.id],
-    ami="ami-0ed62be0fb8004fc6",  # Make sure to replace with the correct AMI for your region
+    ami=ami.id,  # Use the dynamically selected AMI
     subnet_id=subnet_public.id,
     associate_public_ip_address=True,
     key_name="my-keypair")  # Ensure you have this keypair created
