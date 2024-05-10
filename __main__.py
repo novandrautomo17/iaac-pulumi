d5d7be560f099ad6b886ea9066251075596aa58e
Dimas Novandra Utomo <dimasutomo@dimasutomo.com>
2024-05-10 13:09:05 +0700

    remove bastion host"

--- __main__.py
+++ __main__.py
@@ -1,8 +1,8 @@
 import pulumi
 from vpc import vpc, subnet_public, subnet_private_2, subnet_private_1, igw, route_table_private, route_table_assoc_public
-from bastion_host import bastion, bastion_sg
+#from bastion_host import bastion, bastion_sg
 from eks import cluster, node_group
 
 pulumi.export("cluster_name", cluster.name)
 pulumi.export("node_group_name", node_group.node_group_name)
-pulumi.export("bastion_ip", bastion.public_ip)
+#pulumi.export("bastion_ip", bastion.public_ip)
