16a8cdef934d654d3b32bd92e5bb600aa1bd3bf4
Dimas Novandra Utomo <dimasutomo@dimasutomo.com>
2024-05-24 16:42:14 +0700

    fix vpc creation line

--- vpc.py
+++ vpc.py
@@ -10,16 +10,19 @@ vpc = aws.ec2.Vpc("my-vpc",
         "Name": "my-vpc",
     })
 
+# Ensure the VPC creation completes before proceeding
+vpc_id = vpc.id.apply(lambda id: id)
+
 # Create Internet Gateway
 igw = aws.ec2.InternetGateway("my-igw",
-    vpc_id=vpc.id,
+    vpc_id=vpc_id,
     tags={
         "Name": "my-internet-gateway",
     })
 
 # Public Subnet
 subnet_public = aws.ec2.Subnet("my-subnet-public",
-    vpc_id=vpc.id,
+    vpc_id=vpc_id,
     cidr_block="10.0.11.0/24",
     availability_zone="ap-southeast-3a",
     map_public_ip_on_launch=True,
@@ -29,7 +32,7 @@ subnet_public = aws.ec2.Subnet("my-subnet-public",
 
 # Private Subnet (for EKS Node Group)
 subnet_private_1 = aws.ec2.Subnet("subnet-private-1",
-    vpc_id=vpc.id,
+    vpc_id=vpc_id,
     cidr_block="10.0.31.0/24",
     availability_zone="ap-southeast-3a",
     tags={
@@ -38,7 +41,7 @@ subnet_private_1 = aws.ec2.Subnet("subnet-private-1",
     })
 
 subnet_private_2 = aws.ec2.Subnet("subnet-private-2",
-    vpc_id=vpc.id,
+    vpc_id=vpc_id,
     cidr_block="10.0.41.0/24",
     availability_zone="ap-southeast-3b",
     tags={
@@ -48,7 +51,7 @@ subnet_private_2 = aws.ec2.Subnet("subnet-private-2",
 
 # Create a Route Table for Public Subnet
 route_table_public = aws.ec2.RouteTable("my-route-table-public",
-    vpc_id=vpc.id,
+    vpc_id=vpc_id,
     routes=[
         aws.ec2.RouteTableRouteArgs(
             cidr_block="0.0.0.0/0",
@@ -77,7 +80,7 @@ nat_gateway = aws.ec2.NatGateway("my-nat-gateway",
 
 # Create a Route Table for Private Subnets
 route_table_private = aws.ec2.RouteTable("my-route-table-private",
-    vpc_id=vpc.id,
+    vpc_id=vpc_id,
     routes=[
         aws.ec2.RouteTableRouteArgs(
             cidr_block="0.0.0.0/0",
@@ -100,7 +103,7 @@ route_table_assoc_private_2 = aws.ec2.RouteTableAssociation("my-route-table-asso
 
 # Create a security group for the EKS nodes
 eks_node_sg = aws.ec2.SecurityGroup('eks-node-sg',
-    vpc_id=vpc.id,
+    vpc_id=vpc_id,
     description='Security group for EKS nodes',
     ingress=[
         {'protocol': '-1', 'from_port': 0, 'to_port': 0, 'cidr_blocks': ['0.0.0.0/0']},
